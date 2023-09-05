e07f939cbf56d19dbb22e6969ad16583129453c1
lexa56720 <lexa.0606x@gmail.com>
2023-09-05 21:42:15 +0700

    Side panel added

--- TMClient/ViewModel/MainViewModel.cs
+++ TMClient/ViewModel/MainViewModel.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using TMClient.View;
 
 namespace TMClient.ViewModel
 {
-    class MainViewModel
-    {
+    class MainViewModel:BaseViewModel
+    {   
+        public Page SidePanelFrame
+        {
+            get => sidePanelFrame;
+            set
+            {
+                sidePanelFrame = value;
+                OnPropertyChanged(nameof(SidePanelFrame));
+            }
+        }
+        private Page sidePanelFrame;
+
+        public Page MainFrame
+        {
+            get => mainFrame;
+            set
+            {
+                mainFrame = value;
+                OnPropertyChanged(nameof(MainFrame));
+            }
+        }
+        private Page mainFrame;
+
+
+        private SidePanel Panel;
+
         public MainViewModel()
         {
-
+            Panel = new SidePanel();
+            SidePanelFrame = Panel;
         }
     }
 }
