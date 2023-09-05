ab84e35bb881b6b2c699ccfd2db8461310ed71f7
lexa56720 <lexa.0606x@gmail.com>
2023-09-05 23:33:59 +0700

    side panel view improoved

--- TMClient/View/SidePanel.xaml.cs
+++ TMClient/View/SidePanel.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TMClient.ViewModel;
 
 namespace TMClient.View
 {
@@ -23,6 +24,7 @@ namespace TMClient.View
         public SidePanel()
         {
             InitializeComponent();
+            DataContext = new SidePanelViewModel();
         }
     }
 }
---
+++ TMClient/ViewModel/SidePanelViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMClient.Types;
+
+namespace TMClient.ViewModel
+{
+    class SidePanelViewModel : BaseViewModel
+    {
+        public ObservableCollection<Friend> Friends
+        {
+            get => friends;
+            set
+            {
+                friends = value;
+                OnPropertyChanged(nameof(Friends));
+            }
+        }
+
+        private ObservableCollection<Friend> friends = new ObservableCollection<Friend>();
+
+        public SidePanelViewModel()
+        {
+            for (int i = 0; i < 20; i++)
+                Friends.Add(new Friend() { Name = "Ffff" });
+        }
+
+    }
+}
