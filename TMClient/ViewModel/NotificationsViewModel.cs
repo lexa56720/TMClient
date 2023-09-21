be09b53e6f19407780640376ed0d9c2f8f494fc7
lexa56720 <lexa.0606x@gmail.com>
2023-09-21 19:26:44 +0700

    Notification page desing

---
+++ TMClient/ViewModel/NotificationsViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMClient.Types;
+
+namespace TMClient.ViewModel
+{
+    class NotificationsViewModel : BaseViewModel
+    {
+        public ObservableCollection<FriendRequest> FriendRequests { get; set; } = new();
+
+        public NotificationsViewModel()
+        {
+            for (int i = 0; i < 10; i++)
+                FriendRequests.Add(new FriendRequest(new User()
+                {
+                    Id = 1,
+                    IsOnline = true,
+                    Login = "ramus",
+                    Name = "Halif"
+                }, 10));
+        }
+    }
+}
