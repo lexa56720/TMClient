5783cdfa4b6ec6c14ebf22a8843de03d351b1d87
lexa56720 <lexa.0606x@gmail.com>
2024-04-02 19:21:19 +0700

    fix #29

---
+++ TMClient/Controls/SelectableButton.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMClient.Controls
+{
+    using ClientApiWrapper.Types;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    namespace TMClient.Controls
+    {
+        /// <summary>
+        /// Логика взаимодействия для SelectableButton.xaml
+        /// </summary>
+        public partial class SelectableButton : IconButton
+        {
+            public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register(nameof(IsSelected),
+                                        typeof(bool),
+                                        typeof(SelectableButton),
+                                        new PropertyMetadata(false));
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+
+            public bool IsSelected
+            {
+                get => (bool)GetValue(IsSelectedProperty);
+                set
+                {
+                    SetValue(IsSelectedProperty, value);
+                }
+            }
+
+            public SelectableButton()
+            {
+                InitializeComponent();
+                Style = (Style)FindResource("ButtonTemplate");
+            }
+
+            private void IconButton_Click(object sender, RoutedEventArgs e)
+            {
+                IsSelected = true;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelected)));
+            }
+        }
+    }
+
+}
---
+++ TMClient/Utils/Converters/SearchConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TMClient.Utils.Converters
+{
+    class SearchConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2 || values.Last() is not string query || string.IsNullOrWhiteSpace(query))
+                return Visibility.Visible;
+
+            bool isFound = false;
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                if (values[i] is string item && item.Contains(query, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    isFound = true;
+                    break;
+                }
+            }
+            return isFound ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
